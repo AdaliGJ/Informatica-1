71ffd791e1f4ab1969c075215635f4a0771825b9
Adali <garran191137@unis.edu.gt>
2019-09-17 08:05:01 -0600

    Laboratorio 7

--- Laboratorio 7/src/Main.elm
+++ Laboratorio 7/src/Main.elm
@@ -6,40 +6,40 @@ module Main exposing(..)
 
 
 --Ejercicio #1
-type Arbol = Vacio | Constructor Int Arbol Arbol 
+type Arbol = Nil | Cons Int Arbol Arbol 
 
 
 --Ejercicio #2
 masUno: Arbol->Arbol
 masUno arb = case arb of
-    Vacio->Vacio
-    Constructor x arb1 arb2 -> Constructor (x+1) (masUno arb1) (masUno arb2)
+    Nil->Nil
+    Cons x arb1 arb2 -> Cons (x+1) (masUno arb1) (masUno arb2)
 
 
 --Ejercicio #3
 map: (Int->Int)->Arbol->Arbol
 map f arb = case arb of
-    Vacio->Vacio
-    Constructor x arb1 arb2 -> Constructor (f x) (map f arb1) (map f arb2)
+    Nil->Nil
+    Cons x arb1 arb2 -> Cons (f x) (map f arb1) (map f arb2)
 
 
 --Ejercicio #4
 sum: Arbol->Int
 sum arb = case arb of
-    Vacio->0    
-    Constructor x arb1 arb2 -> x + (sum arb1) + (sum arb2)
+    Nil->0    
+    Cons x arb1 arb2 -> x + (sum arb1) + (sum arb2)
 
 
 --Ejercicio #5
 foldTree: (Int->Int->Int->Int)->Int->Arbol->Int
 foldTree f inicial arb = case arb of
-    Vacio->inicial
-    Constructor x arb1 arb2 -> f x (foldTree f inicial arb1) (foldTree f inicial arb2) 
+    Nil->inicial
+    Cons x arb1 arb2 -> f x (foldTree f inicial arb1) (foldTree f inicial arb2) 
 
 
 --Complementos utilizados para pruebas
-xg = Constructor 3 (Constructor 1 Vacio Vacio) Vacio
-mg = Constructor 9 (Constructor 7 (Constructor 5 Vacio Vacio) Vacio) (Constructor 2 Vacio Vacio)
+xg = Cons 3 (Cons 1 Nil Nil) Nil
+mg = Cons 9 (Cons 7 (Cons 5 Nil Nil) Nil) (Cons 2 Nil Nil)
 mas2 x = x+2
 mas x y z = x+y+z
 por x y z =x*y*z
